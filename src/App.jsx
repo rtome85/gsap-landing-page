30489d131f4ec26a7b5de8199971d9a9f74c85e4
robtome85 <rob.tome@gmail.com>
2026-01-17 17:04:58 +0000

    Implement Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div className='flex-center'>
-            <h1 className='text-4xl text-black font-bold'>Hello World</h1>
-        </div>
+        <main>
+            <Navbar />
+        </main>
     )
 }
 
