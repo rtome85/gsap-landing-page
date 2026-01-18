a8373f679703e4618945bd9782519d13bd61e9ca
rtome85 <rob.tome@gmail.com>
2026-01-18 12:28:33 +0000

    Implement Hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/all";
+import { SplitText, ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+    const videoTimelineRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' })
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' })
@@ -35,6 +41,30 @@ const Hero = () => {
         })
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top' : 'bottom top'
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
+
+        return () => {
+            heroSplit.revert();
+            paragraphSplit.revert();
+        }
     }, [])
 
     return (
@@ -42,12 +72,12 @@ const Hero = () => {
             <section id="hero" className="noisy">
                 <h1 className="title uppercase">Mojito</h1>
                 <img
-                    src="images/hero-left-leaf.png"
+                    src="/images/hero-left-leaf.png"
                     alt="left-leaf"
                     className="left-leaf"
                 />
                 <img
-                    src="images/hero-right-leaf.png"
+                    src="/images/hero-right-leaf.png"
                     alt="right-leaf"
                     className="right-leaf"
                 />
@@ -70,6 +100,16 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     );
 };
